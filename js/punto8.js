1d2106fe3d67ce27ee97eaa8adfdf78382a82a51
maurochayle <38973588@fi.unju.edu.ar>
2025-04-15 22:30:14 -0300

    Modificación de los archivos 'Punto8.html' y 'punto8.js', se modificó alert por console.log

--- js/punto8.js
+++ js/punto8.js
@@ -1,12 +1,12 @@
 let calcularMayor=(num1,num2)=> {
     if (num1 > num2){
-        alert("El número mayor entre "+ num1 + " y "+ num2  + " es: "+ num1);
+        console.log("El número mayor entre "+ num1 + " y "+ num2  + " es: "+ num1);
     }
     else if (num2>num1){
-        alert("El número mayor entre "+ num1 + " y "+ num2 + " es: "+ num2);
+        console.log("El número mayor entre "+ num1 + " y "+ num2 + " es: "+ num2);
     }
     else{
-        alert("Tanto "+ num1 + " como "+ num2 +" son iguales.");
+        console.log("Tanto "+ num1 + " como "+ num2 +" son iguales.");
     }
 
 }
