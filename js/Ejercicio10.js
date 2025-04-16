b373f580b9014e476bdda29c8b15753eb11da6c4
anahivelazquez1 <veemilce016@gmail.com>
2025-04-16 17:22:04 -0300

    Desarrollo ejercicio 10

---
+++ js/Ejercicio10.js
@@ -0,0 +1,8 @@
+let num1 = parseInt (prompt("Ingresa el primer numero entero: "));
+let num2 = parseInt (prompt("Ingresa el segundo numero entero: "));
+let num3 = parseInt (prompt("Ingresa el tercer numero entero: "));
+
+let promedio = (num1 + num2 + num3) / 3;
+
+alert("El promedio es: " + promedio);
+console.log("El promedio calculado es: ", promedio);
