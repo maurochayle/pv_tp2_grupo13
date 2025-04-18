9f6c76dfa00700ec1faf780572af543cc6616bef
Leandro Soza <leancarp0495@gmail.com>
2025-04-18 20:51:17 -0300

    Desarrollo ejercicio 5

---
+++ js/Ejercicio5.js
@@ -0,0 +1,7 @@
+let horas = 5;
+let minutos = 50;
+let rdoHoras = horas*60*60;
+let rdoMinutos = minutos*60;
+
+console.log(`Horas ${horas} en segundos: ${rdoHoras}`);
+console.log("Minutos "+minutos+" en segundos: "+rdoMinutos);
